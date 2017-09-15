496e034bc82911e8a6cce9051f8fde0bf7a9b547
mdicurzio <mickael.dicurzio@gmail.com>
2017-09-15 14:40:41 +0200

    jms repear

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new ArticleBundle\ArticleBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
 
         ];
 
--- src/ArticleBundle/Entity/Article.php
+++ src/ArticleBundle/Entity/Article.php
@@ -130,4 +130,3 @@ class Article
         return $this->content;
     }
 }
-
